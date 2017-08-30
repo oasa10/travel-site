92f2cd23806e871ce8be6332673b43e427f1d9dd
oasa10 <oasa1010@gmail.com>
2017-08-30 23:33:48 +0200

    Work with npm packages and gulp

---
+++ gulpfile.js
@@ -0,0 +1,28 @@
+var gulp = require('gulp'),
+watch = require('gulp-watch');
+
+gulp.task('default', function() {
+	console.log("Hooray - you created a Gulp task.");
+});
+
+gulp.task('html', function() {
+	console.log("Imagine something useful veing done to you HTML here");
+});
+
+gulp.task('styles', function() {
+	return gulp.src('./app/assets/styles/styles.css')
+	.pipe()
+	.pipe(gulp.dest('./app/temp/styles'));
+});
+
+gulp.task('watch', function() {
+
+	watch('./app/index.html', function() {
+		gulp.start('html');
+	});
+
+	watch('./app/assets/styles/**/*.css', function() {
+		gulp.start('styles');
+	});
+
+});
